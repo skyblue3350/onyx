9df01ab8070cfb96fd77f71461b218a7cd2982b5
sky <skyblue3350@gmail.com>
2020-03-17 00:31:57 +0900

    implemente AddOrganization component

--- src/renderer/components/addOrganization.tsx
+++ src/renderer/components/addOrganization.tsx
@@ -1,18 +1,31 @@
 import * as React from 'react';
 import { Modal, Button, Form, Input } from 'semantic-ui-react';
+import { Organization } from '../../types/config';
 
 export interface Props {
     open: boolean
     onClose?: () => void
-    onSubmit?: () => void
+    onSubmit?: (org: Organization) => void
 }
 
-export interface State {
+export interface State extends Organization {
 }
 
 export default class AddOrganization extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props)
+
+        this.state = {
+            name: '',
+            api: '',
+            accessToken: '',
+            interval: 3,
+        }
+    }
+
+    onSubmit() {
+        console.log(this.state)
+        if (this.props.onSubmit) this.props.onSubmit(this.state)
     }
 
     render(): JSX.Element {
@@ -25,22 +38,22 @@ export default class AddOrganization extends React.Component<Props, State> {
                     <Modal.Content>
                         <Form>
                         <Form.Field>
-                                <Input label='Name' placeholder='All Issue' fluid/>
+                                <Input value={this.state.name} onChange={(e, data) => this.setState({name: data.value})} label='Name' placeholder='All Issue' fluid/>
                             </Form.Field>
                             <Form.Field>
-                                <Input label='Github API' placeholder='api.github.com' fluid/>
+                                <Input value={this.state.api} onChange={(e, data) => this.setState({api: data.value})} label='Github API' placeholder='api.github.com' fluid/>
                             </Form.Field>
                             <Form.Field>
-                                <Input label='Access Token' placeholder='your access token' fluid/>
+                                <Input value={this.state.accessToken} onChange={(e, data) => this.setState({accessToken: data.value})} label='Access Token' placeholder='your access token' fluid/>
                             </Form.Field>
                             <Form.Field>
-                                <Input label='API Interval' placeholder='10' fluid/>
+                                <Input value={this.state.interval} onChange={(e, data) => this.setState({interval: parseInt(data.value)})} label='API Interval' placeholder='10' fluid/>
                             </Form.Field>
                         </Form>
                     </Modal.Content>
                     <Modal.Actions>
                         <Button onClick={this.props.onClose} negative content='Cancel' />
-                        <Button onClick={this.props.onSubmit} positive content='OK' />
+                        <Button onClick={() => this.onSubmit()} positive content='OK' />
                     </Modal.Actions>
 
                 </Modal>)
